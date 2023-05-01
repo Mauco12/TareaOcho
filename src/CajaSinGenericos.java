dbeeab83fdc74bcd7508928e9bff01bcc073f2f5
Mauricio <mauriciorodriguez90@aragon.unam.mx>
2023-05-01 12:56:29 -0600

    m

--- src/CajaSinGenericos.java
+++ src/CajaSinGenericos.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class CajaSinGenericos {
+import java.util.Arrays;
+
+public class CajaSinGenericos {
+    Object[] content;
+
+    public CajaSinGenericos() {
+    }
+
+    public CajaSinGenericos(Object[] content) {
+        this.content = content;
+    }
+
+    public void add(int pos , Object objeto){
+        content[pos] = objeto;
+    }
+    public Object get(int pos){
+        return content[pos];
+    }
+
+    @Override
+    public String toString() {
+        return "CajaSinGenericos{" +
+                "content=" + Arrays.toString(content) +
+                '}';
+    }
 }
